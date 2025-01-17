872bab15b9875e95620645681f66d028bf2a4de1
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-17 01:08:04 -0500

    Add starter code for Gin HTTP server

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	// Uncomment this block to pass the first stage
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	r.Run(":4221") // Start the server on port 4221
 }
