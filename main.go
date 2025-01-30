a7c04a976a5d77a63f27442f2961a2f110b9cef2
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-29 23:10:34 -0500

    Add getAlbums function

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 // album represents data about a record album.
@@ -19,6 +22,11 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums) //serialize the struct into JSON and add it to the response.
 }
 
 func main() {
