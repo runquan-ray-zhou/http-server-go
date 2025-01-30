1f1c66548569f78c5aa68ff79f14feb2bfc2d4d6
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-29 23:22:38 -0500

    Add getAlbumByID function and GET route to get Album by Id

--- main.go
+++ main.go
@@ -23,10 +23,11 @@ var albums = []album{
 
 func main() {
 
-	router := gin.Default()            //Initialize a Gin router using Default.
-	router.GET("/albums", getAlbums)   //Use the GET function to associate the GET HTTP method and /albums path with a handler function.
-	router.POST("/albums", postAlbums) //Associate the POST method at the /albums path with the postAlbums function.
-	router.Run("127.0.0.1:8080")       //Use the Run function to attach the router to an http.Server and start the server.
+	router := gin.Default()                 //Initialize a Gin router using Default.
+	router.GET("/albums", getAlbums)        //Use the GET function to associate the GET HTTP method and /albums path with a handler function.
+	router.POST("/albums", postAlbums)      //Associate the POST method at the /albums path with the postAlbums function.
+	router.GET("/albums/:id", getAlbumByID) //Associate the /albums/:id path with the getAlbumByID function. In Gin, the colon preceding an item in the path signifies that the item is a path parameter.
+	router.Run("127.0.0.1:8080")            //Use the Run function to attach the router to an http.Server and start the server.
 
 	// l, err := net.Listen("tcp", "127.0.0.1:4221")
 	// if err != nil {
@@ -62,4 +63,20 @@ func postAlbums(c *gin.Context) {
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)            //Append the album struct initialized from the JSON to the albums slice.
 	c.IndentedJSON(http.StatusCreated, newAlbum) //Add a 201 status code to the response, along with JSON representing the album you added.
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id") //Use Context.Param to retrieve the id path parameter from the URL. When you map this handler to a path, you’ll include a placeholder for the parameter in the path.
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a) //If it’s found, you serialize that album struct to JSON and return it as a response with a 200 OK HTTP code.
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"}) //Return an HTTP 404 error with http.StatusNotFound if the album isn’t found.
 }
